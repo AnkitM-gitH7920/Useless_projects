1384eb27bd4c2d0efc3897cce5176b8086e9bae0
AnkitM-gitH7920 <ankitmehra7920@gmail.com>
2025-03-09 12:25:19 +0530

    added changes

--- Spotify Clone/js/script.js
+++ Spotify Clone/js/script.js
@@ -1,3 +1,4 @@
+// getting all the songs
 async function getSongs(){
 	let a = await fetch('songs/songInfo.JSON');
 	let response = await a.json();
@@ -10,6 +11,22 @@ async function getSongs(){
 }
 async function main(){
 	let getSongList = await getSongs();
-	console.log(getSongList)
+	const songListLibrary = document.querySelector('.leftBottom-libraryContent')
+	let currentSong = [];
+	for(let listedSongIndex = 0;listedSongIndex<getSongList.length;listedSongIndex++){
+		songListLibrary.innerHTML += `
+		<div class="song alighnCenter">
+			<img src="assets/music.svg">
+			<div class="songDetails flexDirectionColumn">
+				<div id="songName"><p>${getSongList[listedSongIndex].Song_name}</p></div>
+				<div id="artistName"><p>${getSongList[listedSongIndex].Artist}</p></div></div>
+				<i class='bx bx-play-circle' style='color:#ffffff;cursor: pointer;'  ></i>
+			</div>`
+	}
+	for(song of getSongList){
+		
+	}
 }
 main()
+
+// getting all the songs
