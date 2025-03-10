c7b84cb97d724d2fb23e737da40d924cc00c3a6b
AnkitM-gitH7920 <ankitmehra7920@gmail.com>
2025-03-10 12:25:05 +0530

    added changes

--- Spotify Clone/js/script.js
+++ Spotify Clone/js/script.js
@@ -1,32 +1,43 @@
 // getting all the songs
 async function getSongs(){
 	let a = await fetch('songs/songInfo.JSON');
-	let response = await a.json();
-	let songs = []
-	for(let i = 0; i<response.length ; i++){
-		let ifEndsWithUrl = response[i];
-		songs.push(response[i])
-	}
-	return songs;
+	return a.json()
 }
-async function main(){
-	let getSongList = await getSongs();
-	const songListLibrary = document.querySelector('.leftBottom-libraryContent')
-	let currentSong = [];
-	for(let listedSongIndex = 0;listedSongIndex<getSongList.length;listedSongIndex++){
-		songListLibrary.innerHTML += `
-		<div class="song alighnCenter">
-			<img src="assets/music.svg">
-			<div class="songDetails flexDirectionColumn">
-				<div id="songName"><p>${getSongList[listedSongIndex].Song_name}</p></div>
-				<div id="artistName"><p>${getSongList[listedSongIndex].Artist}</p></div></div>
-				<i class='bx bx-play-circle' style='color:#ffffff;cursor: pointer;'  ></i>
-			</div>`
-	}
-	for(song of getSongList){
-		
-	}
-}
-main()
-
 // getting all the songs
+let getSongList = [];
+async function displaySongsOnLibrary(){
+	const songListLibrary = document.querySelector('.leftBottom-libraryContent');
+	getSongList = await getSongs();
+	getSongList.forEach((song,index)=>{
+		songListLibrary.innerHTML += `
+		<div class="song alighnCenter" onclick="playSong()">
+			<div class="librarySongs-coverImage">
+			    <img src="${song.song_cover}">
+			</div>
+			<div class="songDetails flexDirectionColumn">
+                <div id="songName"><p>${song.Song_name}</p></div>
+			    <div id="artistName"><p>${song.Artist}</p></div>
+			</div>
+			<div class="togglePlayPauseDiv">
+			    <i class='bx bx-play-circle' style='color:#ffffff;cursor: pointer;' song_index="${index}"></i>
+			    <img src="">
+			</div>
+		</div>`;
+	});
+}
+displaySongsOnLibrary();
+function playSong(){
+	let index = event.target.getAttribute('song_index');
+    function playSelectedSong(track){
+        currentAudio = null;
+        document.querySelector(".nextSongClass").addEventListener('click',()=>{
+            currentAudio.pause();
+        })
+        document.querySelector(".prevSongClass").addEventListener('click',()=>{
+            currentAudio.play();
+        })
+	    currentAudio = new Audio(track)
+	    currentAudio.play()
+       };
+       playSelectedSong(getSongList[index].url)
+}
