ff460397dde3e5d216aff901ce08814c2d3e5247
AnkitM-gitH7920 <ankitmehra7920@gmail.com>
2025-03-13 10:52:01 +0530

    added changes

--- Spotify Clone/js/script.js
+++ Spotify Clone/js/script.js
@@ -1,38 +1,101 @@
-let leftBottomLibrary = document.querySelector('.leftBottom-libraryContent')
+let leftBottomLibrary            = document.querySelector('.leftBottom-libraryContent');
 let rightBottomSongCardContainer = document.querySelector('.playlistDesign-songCardContainer');
-console.log(rightBottomSongCardContainer)
 
-async function getSongs(){
-	let songs = await fetch('songs/songInfo.JSON')
-	return await songs.json()
+// global variables
+let card;
+let playbarBtn;
+// global variables
+
+let songs;
+let currentAudio = null;
+
+async function getSongs() {
+	let importedSongs = await fetch('songs/songInfo.JSON');
+	songs = await importedSongs.json();
+	return songs;
 }
-async function displayToLibrary(){
-	let pushedSongs = await getSongs()
-	pushedSongs.forEach((song)=>{
+async function displayToLibraries() {
+	songs = await getSongs();
+	songs.forEach((song) => {
 		leftBottomLibrary.innerHTML += `
 	    <div class="song alighnCenter">
 		    <div class="librarySongs-coverImage">
 			    <img src="${song.song_cover}">
 		    </div>
 		    <div class="songDetails flexDirectionColumn">
-                <div id="songName"><p>${song.Song_name}</p></div>
-			    <div id="artistName"><p>${song.Artist}</p></div>
+                <div id="songName"><span class="flex">${song.Song_name}</span></div>
+			    <div id="artistName"><span class="flex">${song.Artist}</span></div>
 		    </div>
-		    <div class="togglePlayPauseDiv completeCenter">
-			    <i class='bx bx-play-circle' style='color:#ffffff;cursor: pointer;'></i>
+		    <div class="playNowDiv completeCenter">
+			    <p class="playNowButtons">Play Now</p>
 		    </div>
-	    </div>`
+	    </div>`;
 	    rightBottomSongCardContainer.innerHTML += `
 	    <div class="card flexDirectionColumn">
 			<img class="coverImage" src="${song.song_cover}">
 			<button class="appearingPlayBtn completeCenter"><img src="assets/cardPlayButton.svg"></button>
 			<h2>${song.Song_name}</h2>
 			<p>${song.Artist}</p>
-		</div>
-	    `
+		</div>`;
+	});
+	addAnimationToBigText();
+}
+// added animation to big words(no relation with async functions)
+function addAnimationToBigText(){
+	document.querySelectorAll('#songName span').forEach(el =>{
+		let parent = el.parentElement;
+		if(el.scrollWidth>parent.clientWidth){
+			el.classList.add('marqueeAnimation')
+		};
+	});
+	return;
+}
+// added animation to big words(no relation with async functions)
+async function playSong() {
+	let leftLibraryPlayButtons = document.querySelectorAll('.playNowButtons');
+	let rightLibraryPlayButtons = document.querySelectorAll('.appearingPlayBtn');
+
+	leftLibraryPlayButtons.forEach((button, songIndex) => {
+		button.addEventListener('click', () => {
+			playTrack(songs[songIndex].url);
+		});
+	});
+	rightLibraryPlayButtons.forEach((i, songIndex) => {
+		i.addEventListener('click', () => {
+			playTrack(songs[songIndex].url);
+		});
 	});
 }
-async function main(){
-	await displayToLibrary()
+
+function playTrack(track) {
+    playbarBtn = document.getElementById('playSongId');
+	try {
+		if (currentAudio) {
+			currentAudio.pause();
+		}
+		currentAudio = new Audio(track);
+		currentAudio.play();
+		playbarBtn.src = "assets/playBarSongPauseButton.svg";
+
+		// main function , preventing two songs to play at a time
+		playbarBtn.onclick =()=> {
+			if (currentAudio.paused) {
+				currentAudio.play();
+				playbarBtn.src = "assets/playBarSongPauseButton.svg";
+			} else {
+				currentAudio.pause();
+				playbarBtn.src = "assets/playBarSongPlayButton.svg";
+			}
+		};
+		// main function , preventing two songs to play at a time
+	}catch(error) {
+		console.log(error);
+	};
+};
+
+async function main() {
+	await getSongs();
+	await displayToLibraries();
+	await playSong();
 }
-main()
+main();
